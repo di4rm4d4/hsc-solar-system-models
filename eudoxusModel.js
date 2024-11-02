036e8ef238f1690487fc1ca9beb1be314b96fb1c
di4rm4d4 <mcdermott.two@gmail.com>
2024-11-02 22:29:58 +0100

    Update eudoxusModel.js

--- eudoxusModel.js
+++ eudoxusModel.js
@@ -1,5 +1,4 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js';
-import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/controls/OrbitControls.js';
 
 // Parameters for Eudoxus's Model
 const params = {
